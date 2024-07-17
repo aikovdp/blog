6925f4740659927a9785af22d86fcc4f116ea234
aikovdp <aiko@aikovdp.me>
2024-07-17 15:49:06 +0200

    Add redirects to cover 404s

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,10 @@ import remarkToc from "remark-toc";
 export default defineConfig({
   site: "https://aiko.wtf",
   integrations: [tailwind()],
+  redirects: {
+    "/": "/blog",
+    "/blog": "/blog/gradle-wrapper", // Until multiple posts have been written
+  },
   markdown: {
     remarkPlugins: [remarkToc],
   },
